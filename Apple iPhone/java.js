36ad4190be1e72ab7ba2b843b9c55fc669244dab
Gordijonok <126864195+Gordijonok@users.noreply.github.com>
2023-09-02 12:52:52 +0300

    one com

---
+++ Apple iPhone/java.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+
+  const btn = document.querySelector(".menu-btn");
+  const nav = document.querySelector(".nav");
+
+  btn.addEventListener("click", () => {
+    nav.classList.toggle("menu-open");
+  });
+})();
